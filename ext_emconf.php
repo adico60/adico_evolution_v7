34ad13545db087feb8768a71a0b0044284013c12
YaNn <yann.berton@adico.fr>
2016-09-02 10:48:12 +0200

    Initial Commit
    Adaptation adico_evolution en version 7.6 de typo3

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pradinestemplate".
+ *
+ * Auto generated 23-08-2014 13:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Adico theme Evolution V7',
+	'description' => 'Theme Evolution pour plateforme 7.6',
+	'category' => 'fe',
+	'version' => '0.0.1',
+	'state' => 'stable',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Yann Berton',
+	'author_email' => 'yann.berton@adico.fr',
+	'author_company' => 'ADICO',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '7.6.2 - 8.99.99'
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
